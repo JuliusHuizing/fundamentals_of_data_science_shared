34512b2610441ef200a144c2e26381c2d7e1eeac
Julius <j.b.huizign@gmail.com>
2023-09-29 15:41:13 +0200

    fix bugs.

--- notebooks/disaster_data/utils/disaster_data_utils.py
+++ notebooks/disaster_data/utils/disaster_data_utils.py
@@ -4,13 +4,13 @@ import os
 current_path = os.path.dirname(os.path.realpath(__file__))
 root_path = f"{current_path}/../../.."
 
-
-
 def build_clean_dataframe() -> pd.DataFrame:
     df = build_dataframe()
-    df = df.dropna()
     df = remove_irrelevant_disasters(df)
     df = remove_irrelevant_columns(df)
+    # only drop rows with missing values after deleting all irelevant columns because there are a lot of columns with missing values in general.
+    df = df.dropna()
+
     return df
 
 def build_dataframe() -> pd.DataFrame:
@@ -29,7 +29,6 @@ def remove_irrelevant_disasters(df) -> pd.DataFrame:
     df = df[df["causes climate change sentiment"] == True]
     return df
  
-
 def remove_irrelevant_columns(df) -> pd.DataFrame:
     columns_of_interest = ["Country", "Total Deaths", "Year", "Disaster Type"]
     df = df[columns_of_interest]
